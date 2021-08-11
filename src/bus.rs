916a226ec55e4d804c092ec01a77d89148d037b9
vmlinuz719 <61648059+vmlinuz719@users.noreply.github.com>
2021-08-10 22:39:05 -0400

    Add a new Bus that supports multiple attached devices

--- src/bus.rs
+++ src/bus.rs
@@ -1,27 +1,171 @@
 use std::sync::{Arc, Mutex, Condvar};
 
+// Memory32 trait for use with bus, as well as reference impl for Vec<u8>
+
+#[derive(Debug)]
+#[allow(dead_code)]
+pub enum BusError {
+	AccessViolation,
+	AlignmentCheck,
+	InvalidAddress
+}
+
+pub trait Memory32<A, E> {
+	fn read_b(&self, addr: A) -> Result<u8, E>;
+	fn read_h(&self, addr: A) -> Result<u16, E>;
+	fn read_w(&self, addr: A) -> Result<u32, E>;
+	
+	fn write_b(&mut self, addr: A, data: u8) -> Result<(), E>;
+	fn write_h(&mut self, addr: A, data: u16) -> Result<(), E>;
+	fn write_w(&mut self, addr: A, data: u32) -> Result<(), E>;
+}
+
+impl Memory32<u32, BusError> for Vec<u8> {
+	fn read_b(&self, addr: u32) -> Result<u8, BusError> {
+		if addr >= self.len() as u32 {
+			Err(BusError::InvalidAddress)
+		} else {
+			Ok(self[addr as usize])
+		}
+	}
+	fn read_h(&self, addr: u32) -> Result<u16, BusError> {
+		if addr + 1 >= self.len() as u32 {
+			Err(BusError::InvalidAddress)
+		} else if addr % 2 != 0 {
+			Err(BusError::AlignmentCheck)
+		} else {
+			Ok(((self[(addr + 1) as usize] as u16) << 8) + (self[addr as usize] as u16))
+		}
+	}
+	fn read_w(&self, addr: u32) -> Result<u32, BusError> {
+		if addr + 3 >= self.len() as u32 {
+			Err(BusError::InvalidAddress)
+		} else if addr % 4 != 0 {
+			Err(BusError::AlignmentCheck)
+		} else {
+			Ok(((self[(addr + 3) as usize] as u32) << 3) + ((self[(addr + 2) as usize] as u32) << 8)
+				+ ((self[(addr + 1) as usize] as u32) << 8) + (self[addr as usize] as u32))
+		}
+	}
+	
+	fn write_b(&mut self, addr: u32, data: u8) -> Result<(), BusError> {
+		if addr >= self.len() as u32 {
+			Err(BusError::InvalidAddress)
+		} else {
+			self[addr as usize] = data;
+			Ok(())
+		}
+	}
+	fn write_h(&mut self, addr: u32, data: u16) -> Result<(), BusError> {
+		if addr + 1 >= self.len() as u32 {
+			Err(BusError::InvalidAddress)
+		} else if addr % 2 != 0 {
+			Err(BusError::AlignmentCheck)
+		} else {
+			self[(addr + 1) as usize] = ((data >> 8) & 0xFF) as u8;
+			self[addr as usize] = (data & 0xFF) as u8;
+			Ok(())
+		}
+	}
+	fn write_w(&mut self, addr: u32, data: u32) -> Result<(), BusError> {
+		if addr + 3 >= self.len() as u32 {
+			Err(BusError::InvalidAddress)
+		} else if addr % 4 != 0 {
+			Err(BusError::AlignmentCheck)
+		} else {
+			self[(addr + 3) as usize] = ((data >> 24) & 0xFF) as u8;
+			self[(addr + 2) as usize] = ((data >> 16) & 0xFF) as u8;
+			self[(addr + 1) as usize] = ((data >> 8) & 0xFF) as u8;
+			self[addr as usize] = (data & 0xFF) as u8;
+			Ok(())
+		}
+	}
+}
+
+//
+
 pub struct Bus {
-	address: u32,
-	data: u32
+	base: Vec<u32>,
+	size: Vec<u32>,
+	region: Vec<Arc<Mutex<dyn Memory32<u32, BusError>>>>
 }
 
 impl Bus {
 	pub fn new() -> Bus {
 		Bus {
-			address: 0,
-			data: 0
+			base: Vec::new(),
+			size: Vec::new(),
+			region: Vec::new()
 		}
 	}
 	
-	pub fn set_address(&mut self, address: u32) {
-		self.address = address;
-	}
-	
-	pub fn read(&self) -> u32 {
-		println!("Bus: Read 0x{:08X}", self.address);
-		return self.data;
+	pub fn attach(&mut self, base: u32, size: u32,
+		region: Arc<Mutex<dyn Memory32<u32, BusError>>>) {
+		self.base.push(base);
+		self.size.push(size);
+		self.region.push(region);
 	}
 }
+
+impl Memory32<u32, BusError> for Bus {
+	fn read_b(&self, addr: u32) -> Result<u8, BusError> {
+		for n in 0..self.base.len() {
+			if addr >= self.base[n] && addr < self.base[n] + self.size[n] {
+				let mem = self.region[n].lock().unwrap();
+				return mem.read_b(addr - self.base[n]);
+			}
+		}
+		return Err(BusError::InvalidAddress);
+	}
+	fn read_h(&self, addr: u32) -> Result<u16, BusError> {
+		for n in 0..self.base.len() {
+			if addr >= self.base[n] && addr < self.base[n] + self.size[n] {
+				let mem = self.region[n].lock().unwrap();
+				return mem.read_h(addr - self.base[n]);
+			}
+		}
+		return Err(BusError::InvalidAddress);
+	}
+	fn read_w(&self, addr: u32) -> Result<u32, BusError> {
+		for n in 0..self.base.len() {
+			if addr >= self.base[n] && addr < self.base[n] + self.size[n] {
+				let mem = self.region[n].lock().unwrap();
+				return mem.read_w(addr - self.base[n]);
+			}
+		}
+		return Err(BusError::InvalidAddress);
+	}
+	
+	fn write_b(&mut self, addr: u32, data: u8) -> Result<(), BusError> {
+		for n in 0..self.base.len() {
+			if addr >= self.base[n] && addr < self.base[n] + self.size[n] {
+				let mut mem = self.region[n].lock().unwrap();
+				return mem.write_b(addr - self.base[n], data);
+			}
+		}
+		return Err(BusError::InvalidAddress);
+	}
+	fn write_h(&mut self, addr: u32, data: u16) -> Result<(), BusError> {
+		for n in 0..self.base.len() {
+			if addr >= self.base[n] && addr < self.base[n] + self.size[n] {
+				let mut mem = self.region[n].lock().unwrap();
+				return mem.write_h(addr - self.base[n], data);
+			}
+		}
+		return Err(BusError::InvalidAddress);
+	}
+	fn write_w(&mut self, addr: u32, data: u32) -> Result<(), BusError> {
+		for n in 0..self.base.len() {
+			if addr >= self.base[n] && addr < self.base[n] + self.size[n] {
+				let mut mem = self.region[n].lock().unwrap();
+				return mem.write_w(addr - self.base[n], data);
+			}
+		}
+		return Err(BusError::InvalidAddress);
+	}
+}
+
+// Channel - a generic synchronization construct
 
 pub struct Channel<T> {
 	bus: Arc<Mutex<T>>,
